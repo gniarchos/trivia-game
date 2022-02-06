f2179a75facff11df87d030209ca699bd284b847
gniarchos <giannisnrch@gmail.com>
2022-02-06 19:34:38 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,183 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Start from './components/Start'
+import Background from "./components/Background"
+import Quiz from './components/Quiz'
+import Button from './components/Button'
+import {nanoid} from "nanoid"
+import arrayShuffle from 'array-shuffle';
+import Confetti from 'react-confetti'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+
+    const [isStarted, setIsStarted] = React.useState(false)
+    const [gameEnded, setGameEnded] = React.useState(false)
+    const [message, setMessage] = React.useState("")
+    const [finished, setFinished] = React.useState(false)
+    const [loader, setLoader] = React.useState(false)
+    const [number, setNumber] = React.useState(0)
+    const [category, setCategory] = React.useState("any")
+    const [difficulty, setDifficulty] = React.useState("any")
+    const [legend, setLegend] = React.useState(false)
+    const [numberCorrect, setNumberCorrect] = React.useState(0)
+    localStorage.setItem("correctAnswersNum", "null")
+
+    function fetchNumberQuestions(event) {
+        const {value} = event.target
+        if ([value] == "")
+        {
+            setNumber(0)
+        }
+        else
+        {
+            setNumber([value])
+        }
+        
+    }
+
+    function fetchCategory(event) {
+        const {value} = event.target
+        if ([value] == "any")
+        {
+            setCategory("any")
+        }
+        else
+        {
+            setCategory([value])
+        }
+        
+    }
+
+    function fetchDifficulty(event) {
+        const {value} = event.target
+        if ([value] == "any")
+        {
+            setDifficulty("any")
+        }
+        else
+        {
+            setDifficulty([value])
+        }
+        
+    }
+
+    function createNewGame() {
+        console.log("Creating new game. . .")
+        if (number != 0)
+        {
+            setIsStarted(oldIsStarted => oldIsStarted = true) 
+            setLoader(oldLoader => oldLoader = true)
+        }
+          
+        
+    }
+
+    function startNewGame() {
+        console.log("Starting new game. . .")
+        setIsStarted(oldIsStarted => oldIsStarted = !oldIsStarted)
+        setGameEnded(oldGameEnded => oldGameEnded = !oldGameEnded)
+        setNumber(0)  
+        setCategory("any")
+        setDifficulty("any")
+        localStorage.setItem("correctAnswersNum", "null")
+        setNumberCorrect(0)
+    }
+
+    function showResults() {
+        console.log("Showing results. . .")
+        setGameEnded(oldVal => oldVal = true)
+    }
+
+    const [questions, setQuestions] = React.useState([{
+        id: null,
+        question: "", 
+        correct_answer: "", 
+        answers: [], 
+        category: "", 
+        difficulty: "", 
+        type: ""
+    }])
+
+    React.useEffect(() => {
+        setMessage(oldMessage => oldMessage = localStorage.getItem("correctAnswers"))
+        setNumberCorrect(oldNumberCorrect =>  oldNumberCorrect = localStorage.getItem("correctAnswersNum"))
+        // setFinished(prevFinished => prevFinished = localStorage.getItem("finished"))   
+    }, [gameEnded])
+
+    React.useEffect(() => {
+        
+            // fetch(`https://opentdb.com/api.php?amount=${number}`)
+            var api = ""
+
+            if (category === "any")
+            {
+                if (difficulty === "any")
+                {
+                    api = `https://opentdb.com/api.php?amount=${number}`
+                }
+                else 
+                {
+                    api = `https://opentdb.com/api.php?amount=${number}&difficulty=${difficulty}`
+                }
+                
+            }
+            else
+            {
+                if (difficulty === "any")
+                {
+                    api = `https://opentdb.com/api.php?amount=${number}&category=${category}`
+                }
+                else 
+                {
+                    api = `https://opentdb.com/api.php?amount=${number}&category=${category}&difficulty=${difficulty}`
+                }       
+            }
+
+            fetch(api)
+                .then(res => res.json())
+                .then(data => setQuestions(data.results.map(item => ({
+                    key: nanoid(),
+                    question: item.question,
+                    correct_answer: item.correct_answer,
+                    category: item.category,
+                    difficulty: item.difficulty,
+                    type: item.type,
+                    answers: arrayShuffle(item.incorrect_answers.concat(item.correct_answer)),
+                    // handleSelected: handleSelected
+                    
+                }))))
+
+
+            // setLoader(false)
+
+    }, [isStarted])
+
+    // React.useEffect(() => {
+    //     fetch("https://opentdb.com/api.php?amount=3")
+    //     .then(res => res.json())
+    //     .then(data => setQuestions(data.results))
+
+    // }, [])
+
+    // console.log(questions)
+
+    return (
+        <main>     
+            {String(numberCorrect).valueOf() === String(number).valueOf() && <Confetti />}
+            <Background />
+            {isStarted == false && <Start handleClick= {createNewGame} handleChangeNumber={fetchNumberQuestions} handleChangeCategory={fetchCategory} handleChangeDifficulty={fetchDifficulty} numQuestions={number}/>}
+
+            {/* {loader == true && <div className='loader'><BallTriangle color="#293264" height={120} width={120} /></div>} */}
+            {isStarted == true && <Quiz Data={questions} ended={gameEnded} numQuestions={number}/>}
+            {isStarted == true && <Button ended= {gameEnded} startNewGame = {startNewGame} showResults = {showResults} />}
+
+            {gameEnded == true && <div align="center">
+               <h3 className='correct-answers'>{message}</h3>
+               {String(numberCorrect).valueOf() === String(number).valueOf() && <h3 className='correct-answers'>Congratulations! You answered all the questions correct!!</h3>}
+            </div>}
+
+
+            
+
+        </main>
+    )
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -2,16 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// ** TODO:
+// ** √ user set number of questions
+// ** √ user set category
+// ** √ user set difficulty
+// ** - (?) scoreboard
+// ** √ 10/10 confetti
+// ** √ count better the correct answers
+// **
+// **
